8224a39208ca8d7d90aa24fed67b8495d02769df
Sergio Del Castillo <serdelcas1990@gmail.com>
2018-11-05 21:47:32 +0100

    Fixed bug setting cost

--- models/purchase_order.py
+++ models/purchase_order.py
@@ -22,8 +22,15 @@ class Product(models.Model):
     @api.model
     def action_set_costs(self):
         for product_id in self.env['product.template'].search([]):
+            price = 0.0
             have_purchases = self.env['purchase.order.line'].search([('product_id', '=', product_id.id)], order='id desc', limit=1)
-            product_id.standard_price = have_purchases.price_unit
+            if len(have_purchases):
+                price = have_purchases.price_unit
+            else:
+                seller_id = self.env['product.supplierinfo'].search([('product_id', '=', product_id.id)], order='id desc', limit=1)
+                if len(seller_id):
+                    price = product_id.seller_id.price
+            product_id.standard_price = price
 
 class StockScrap(models.Model):
     _inherit = "stock.scrap"
